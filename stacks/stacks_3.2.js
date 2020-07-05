e593e2e5407e3659c53af5c86fae987b79873f1b
Paige Godfrey <paagodfrey@gmail.com>
2020-07-05 14:05:00 -0700

    3.2 updated to optimal

--- stacks/stacks_3.2.js
+++ stacks/stacks_3.2.js
@@ -1,3 +1,4 @@
+// this method stores [value, count] pairs in the min stack to account for duplicates
 class StackWithMin {
   constructor() {
     this.values = [];
@@ -5,22 +6,28 @@ class StackWithMin {
   }
 
   push(val) {
-    // use less than or equal to to account for duplicate min values
-    if (val <= this.min[this.min.length - 1]) this.min.push(val);
-    return this.values.push(val);
+    let minTop = this.min.length - 1;
+
+    if (!this.min.length || val < this.min[minTop[0]]) this.min.push([val, 1]);
+    else if (val === this.min[minTop[0]]) this.min[minTop[1]]++;
+
+    this.values.push(val);
   }
 
   pop() {
-    if (this.values[this.values.length - 1] === this.min[this.min.length - 1]) this.min.pop();
-    return this.values.pop();
+    let minTop = this.min.length - 1;
+
+    if (this.values[this.values.length - 1] === this.min[minTop[0]]) this.min[minTop[1]]--;
+    if (this.min[minTop[1]] === 0) this.min.pop();
+
+    this.values.pop();
   }
 
   peek() {
     return this.values[this.values.length - 1];
   }
 
-  isEmpty() {
-    return this.values.length === 0;
+  getMin() {
+    return this.min[this.min.length - 1[0]];
   }
-
 }
